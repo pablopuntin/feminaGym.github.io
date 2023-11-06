4ad51403d035cebe1e05b787b8c41af591663b99
pablopuntin <pablopuntin@gmail.com>
2023-11-06 19:40:31 -0300

    funcionalidad modo oscuro y form con precios

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Tu código JavaScript aquí
+  // Se ejecutará una vez que el DOM esté completamente cargado
+
+
+
+
+
 /* logica para agregar txto y quitar texto con el mismo boton*/
 let btnInfo = document.getElementById('masInfo');
 let parraro = document.getElementById('texto');
@@ -19,11 +27,68 @@ btnInfo.addEventListener('click', function(){
 let btnOscuro = document.getElementById('oscuro');
 
 btnOscuro.addEventListener('click', function(){
-  if(btnOscuro.textContent === 'Modo claro'){
-    document.body.style.backgroundColor = 'black';
-    btnOscuro.textContent = 'Modo oscuro';
-   } else{
-    document.body.style.backgroundColor = 'white';
+  if(btnOscuro.textContent === 'Modo oscuro'){
+    document.body.style.backgroundColor = 'slategray';
     btnOscuro.textContent = 'Modo claro';
+   } else{
+    document.body.style.backgroundColor = 'floralwhite';
+    btnOscuro.textContent = 'Modo oscuro';
    }
 })
+
+
+
+
+//script para el form de venta
+// definir el valor del ticket
+const ticket = 2000;
+
+// definir descuentos
+const descEstudiante = 0.05;
+const descTrainee = 0.10;
+const descJunior = 0.15;
+
+// tomar los elementos
+
+const cantidad = document.getElementById('cantidad');
+const categoria = document.getElementById('categoria');
+const totalPagar = document.getElementById('totalAPagar');
+const btnResumen = document.getElementById('resumen');
+
+
+function calcularPago() {
+
+    let total = cantidad.value * ticket;
+
+    // condicionar por descuentos
+    switch (categoria.value) {
+        case "estudiante":
+            total = total * 1.2 - (total * descEstudiante);
+            break;
+        case "trainee":
+            total = total * 1.8 - (total * descTrainee);
+            break;
+        case "junior":
+            total = total * 3 - (total * descJunior);
+            break;
+    }
+
+    totalPagar.textContent = `Total a Pagar: $ ${total}`;
+
+    console.log("hola");
+}
+
+btnResumen.addEventListener('click', (e) => {
+    e.preventDefault();
+    calcularPago();
+
+});
+
+//fin script de venta
+
+
+//despues de esta linea el js se carga antes del dom
+
+});
+
+
